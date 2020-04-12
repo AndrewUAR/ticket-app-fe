c901f1cc70b5c3cbb9f6ed70b4b54b65ba1dabf8
Andrew <andrewriznykua93@gmail.com>
2020-04-11 22:34:15 -0500

    validate func import ed and passed into onRegisteUser function

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Auth.css';
 import FormInput from '../reusable/FormInput';
 import RadioInput from '../reusable/RadioInput';
 import Button from '../reusable/Button';
 import { Link } from 'react-router-dom';
+import { validateInputs } from '../../helpers/Helpers';
 
 const Register = () => {
-  const onChange = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    role: ''
+  });
 
+  const [error, setError] = useState({
+    usernameError: '',
+    passwordError: '',
+    roleError: ''
+  })
+
+  const { username, password } = user;
+  const { usernameError, passwordError, roleError } = error;
+
+  const onRegisterUser = e => {
+    e.preventDefault();
+    const isValid = validateInputs(user, setError);
+    console.log(isValid)
+    if (isValid) {
+      console.log(user)
+    }
   }
 
-  const onClick = () => {}
+  const onChange = e => {
+    const { name, value } = e.target;
+    setUser(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
 
   return (
     <div className="auth-wrapper">
       <div className="auth-inner">
-        <form>
+        <form onSubmit={onRegisterUser}>
           <h3>Sign Up</h3>
           <div className="form-group">
             <FormInput
@@ -24,8 +51,8 @@ const Register = () => {
               label="Username"
               className="form-control"
               placeholder="Enter Username"
-              value=""
-              error=""
+              value={username}
+              error={usernameError}
               onChange={onChange}
             />
             <FormInput
@@ -34,8 +61,8 @@ const Register = () => {
               label="Password"
               className="form-control"
               placeholder="Enter Password"
-              value=""
-              error=""
+              value={password}
+              error={passwordError}
               onChange={onChange}
             />
           </div>
@@ -47,7 +74,7 @@ const Register = () => {
                 labelClassName="form-check-label"
                 className="form-check-input"
                 value="User"
-                error=""
+                error={roleError}
                 onChange={onChange}
               />
             </div>
@@ -58,7 +85,7 @@ const Register = () => {
                 labelClassName="form-check-label"
                 className="form-check-input"
                 value="Admin"
-                error=""
+                error={roleError}
                 onChange={onChange}
               />
             </div>
@@ -67,7 +94,6 @@ const Register = () => {
             type="submit"
             label="Sign Up"
             className="btn btn-primary btn-block"
-            omClick={onClick}
           />
           <p className="forgot-password">
             Already registered? <Link to={"/sign-in"}>Login</Link>
