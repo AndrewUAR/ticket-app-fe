45a2b4728eda450a75f37f7506a52757088c37e9
Andrew <andrewriznykua93@gmail.com>
2020-04-12 23:59:12 -0500

    routes wrapped into persistGate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { store, persistor } from './redux/store';
+import Dashboard from './components/main/home/Dashboard';
+import PrivateRoute from './components/private/PrivateRoute';
+import Navbar from './components/layouts/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <Navbar />
+          <div className="App container">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path='/sign-in' component={Login} />
+              <Route path='/sign-up' component={Register} />
+              <PrivateRoute path='/dashboard' component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
