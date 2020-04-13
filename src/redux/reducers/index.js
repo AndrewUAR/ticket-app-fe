c6d5e30ce720ef131c51eb01788f9b8c498586c9
Andrew <andrewriznykua93@gmail.com>
2020-04-12 23:57:50 -0500

    modal and tickets reducers added to rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer'
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+import modalReducer from './modalReducer';
+import ticketReducer from './ticketReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
 
 const rootReducers = combineReducers({
   auth: authReducer,
-  error: ''
-})
+  errors: errorReducer,
+  modal: modalReducer,
+  tickets: ticketReducer
+});
 
-export default rootReducers;
+export default persistReducer(persistConfig, rootReducers);
