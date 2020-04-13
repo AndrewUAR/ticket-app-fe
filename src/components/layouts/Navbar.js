cc991d09a7242a5e1cff59969a7d2bf50003c46c
Andrew <andrewriznykua93@gmail.com>
2020-04-12 23:55:09 -0500

    navbar component created

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Navbar = props => {
+  const { isAuthenticated } = props;
+  const history = useHistory();
+
+  const logoutUser = () => {
+    history.push('/');
+  }
+
+  return (
+    <>
+      {
+        isAuthenticated 
+        ? <div className="navbar">
+            <div className="container">
+              <Link to={'/dashboard'} className="navbar-brand">TicketApp</Link>
+              <div className="collapses navbar-collapses">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a href="#!" onClick={logoutUser}>
+                      <i className="fas fa-sign-out-alt"></i>{' '}
+                      <span>Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div> 
+        : ""
+      }
+    </>
+  )
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Navbar);
+
+Navbar.propTypes = {
+  isAuthenticated: PropTypes.bool
+}
