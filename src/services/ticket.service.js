041eed28a7922ea90d82dc60110748cd84e4f692
Andrew <andrewriznykua93@gmail.com>
2020-04-12 23:51:34 -0500

    service functions for tickets created

---
+++ src/services/ticket.service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_ENDPOINT = 'http://localhost:5000';
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+export const addNewTicket = async (ticketData) => {
+  const response = await axios.post(`${API_ENDPOINT}/tickets`, ticketData, config);
+  return response;
+}
+
+export const getAllTickets = async () => {
+  const response = await axios.get(`${API_ENDPOINT}/tickets`, config);
+  return response;
+}
+
+export const editTicket = async (id, ticketData) => {
+  const response = await axios.patch(`${API_ENDPOINT}/tickets/${id}`, ticketData, config);
+  return response;
+} 
+
+export const deleteTicket = async (id, ticketData) => {
+  const response = await axios.delete(`${API_ENDPOINT}/tickets/${id}`, config);
+  return response;
+}
+
+export const closeTicket = async (id) => {
+  const response = await axios.patch(`${API_ENDPOINT}/tickets/${id}`, config);
+  return response;
+}
